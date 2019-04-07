e5cf7659938ac46b7d18205df4624c16036fc1f8
karmaChamelion <kristjan.arumae@gmail.com>
2019-04-06 21:33:19 -0400

    Aligned original text with OpemNMT attn for labels

--- create_sent_data.py
+++ create_sent_data.py
@@ -1,6 +1,7 @@
 import json
 from nltk.tokenize import sent_tokenize
 import numpy as np
+import io
 
 
 def create_heatmap2():
@@ -12,11 +13,13 @@ def create_heatmap2():
     tgt_list = []
     attn_list = []
     src_list_raw = []
-    num_pos = 0
     len_ls = []
+    batch_idx = []
+
+    num_pos = 0
 
     ofp_json = open('data.nosync/train/cnndm.json', 'w+')
-    data = {'x' : [], 'y' : []}
+    data = {'x': [], 'x_o': [], 'y': []}
 
     for i in xrange(2):
         ifp_model = open('stanford_attn' + str(i), 'rb')
@@ -26,6 +29,7 @@ def create_heatmap2():
             src_ls_sample = sample[0]
             tgt_ls_sample = sample[1]
             attn_ls_sample = sample[2]
+            batch_idx_sample = sample[3]
 
             src_ls_raw = [vocab_map[str(x)] for x in src_ls_sample]
 
@@ -33,26 +37,50 @@ def create_heatmap2():
             tgt_list.append(tgt_ls_sample)
             attn_list.append(attn_ls_sample)
             src_list_raw.append(src_ls_raw)
+            batch_idx.append(batch_idx_sample)
 
         ifp_model.close()
 
-    assert len(tgt_list) == len(src_list)
-    for a_ls, x_ls, y_ls, x_ls_r in zip(attn_list, src_list, tgt_list, src_list_raw):
+    # ifp_orig = open('data.nosync/train.txt.src', 'rb')
+    ifp_orig = io.open("data.nosync/train.txt.src", mode="r", encoding="utf-8")
+    x_orig, y_orig = [], []
+    for i, line_x in enumerate(ifp_orig):
 
+        if i > len(src_list) - 1:
+            break
+        x_orig.append(line_x.rstrip())
+
+    ifp_orig.close()
+
+    print len(tgt_list), len(src_list)
+
+    src_list_raw = reverse_batch(src_list_raw, batch_idx)
+    attn_list = reverse_batch(attn_list, batch_idx)
+    src_list = reverse_batch(src_list, batch_idx)
+    tgt_list = reverse_batch(tgt_list, batch_idx)
+
+    for a_ls, x_ls,  x_ls_r, x_o, y_ls in zip(attn_list, src_list, src_list_raw, x_orig, tgt_list):
         assert len(x_ls) == len(x_ls_r)
 
         most_used_idxs_map = get_most_used(a_ls, y_ls)
+        doc = ' '.join(x_ls_r)
 
-        doc = ' '.join(x_ls_r).encode('utf-8')
-        sentences = sent_tokenize(doc)
+        sentences = sent_tokenize(doc.encode('utf-8'))
+        sentences_orig = sent_tokenize(x_o)
         token_idx = 0
 
-        for sent in sentences:
+        for sent, sent_o in zip(sentences, sentences_orig):
             total_in_sent = 0
-            token_begin = token_idx
             highlight_ls = []
+            s_split = sent.split()
+            s_split_orig = sent_o.split()
 
-            for i, _ in enumerate(sent.split()):
+            if len(s_split) < len(s_split_orig):
+                print '_____', sent
+                print '_____', sent_o
+                break
+
+            for i, _ in enumerate(s_split):
                 if token_idx in most_used_idxs_map:
                     total_in_sent += 1
                     highlight_ls.append(1)
@@ -75,16 +103,47 @@ def create_heatmap2():
             else:
                 single_y.extend([-1, -1])
 
-            data['x'].append(x_ls[token_begin:token_idx].tolist())
+            data['x'].append(sent)
+            data['x_o'].append(sent_o)
             data['y'].append(single_y)
 
-            len_ls.append(len(data['x'][-1]))
+            print 'S', sent
+            print 'O', sent_o
 
+            len_ls.append(len(data['x'][-1].split()))
+        print '\n'
     json.dump(data, ofp_json)
     ofp_json.close()
 
     print num_pos/float(len(data['y'])), len(data['y']), len(data['x'])
     print np.median(len_ls), np.mean(len_ls)
+
+
+def reverse_batch(x, batch_idx_ls, batch_size=128):
+    ret_arr, batched_arr, cur_batch = [], [], []
+    batch_idx = 0
+
+    for item_x, item_b in zip(x, batch_idx_ls):
+
+        if batch_idx < batch_size:
+            cur_batch.append((item_x, item_b))
+            batch_idx += 1
+        else:
+            batched_arr.append(cur_batch)
+
+            cur_batch = []
+
+            cur_batch.append((item_x, item_b))
+            batch_idx = 1
+
+    for batch in batched_arr:
+
+        batch.sort(key=text_sort_key)
+
+        for item, i in batch:
+            ret_arr.append(item)
+
+    return ret_arr
 
 
 def get_most_used(a_ls, y_ls):
@@ -96,6 +155,10 @@ def get_most_used(a_ls, y_ls):
         most_used[np.argmax(sub_ls_a)] = i
 
     return most_used
+
+
+def text_sort_key(ex):
+    return ex[1]
 
 
 def combine_chunks(highlight_ls):
