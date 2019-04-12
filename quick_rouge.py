4fbc75d438b9ebd5f06a840f3dbfaa5bea4d4666
karmaChamelion <kristjan.arumae@gmail.com>
2019-04-12 15:43:38 -0400

    rouge

---
+++ quick_rouge.py
@@ -0,0 +1,32 @@
+from pyrouge import Rouge155
+import tempfile
+
+
+def run_rouge(data_split='valid', by=False, s='system_sent'):
+    tempfile.tempdir = '/scratch/'
+
+    rouge_fname = 'data.nosync/' + data_split + '/model/'
+
+    if by:
+        r = Rouge155(rouge_args='-e /home/kristjan/data1/softwares/rouge/ROUGE/RELEASE-1.5.5/data -c 95 -2 -1 -U -r 1000 -n 4 -w 1.2 -a -m -b 75')
+    else:
+        r = Rouge155()
+
+    r.system_dir = rouge_fname
+    r.model_dir = 'data.nosync/' + data_split + '/' + s + '/'
+
+    r.system_filename_pattern = 'sum.(\d+).txt'
+    r.model_filename_pattern = 'd_#ID#.txt'
+
+    fname = s + '_rouge.out'
+    ofp = open(fname, 'w+')
+
+    ofp.write(r.convert_and_evaluate())
+    ofp.close()
+
+# output_path_model = 'data.nosync/' + data_split + '/model/'
+# output_path_system_sent = 'data.nosync/' + data_split + '/system_sent/'
+# output_path_system_segm = 'data.nosync/' + data_split + '/system_segm/'
+
+run_rouge()
+run_rouge(s='system_segm')
