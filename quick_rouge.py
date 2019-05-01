011b963561892d078dae40ff016f3021c223345e
karmaChamelion <kristjan.arumae@gmail.com>
2019-05-01 14:32:08 -0400

    modeling

--- quick_rouge.py
+++ quick_rouge.py
@@ -2,10 +2,10 @@ from pyrouge import Rouge155
 import tempfile
 
 
-def run_rouge(data_split='test', by=False, s='system_sent'):
+def run_rouge(data_split='train', by=False, s='small_sys_sent'):
     tempfile.tempdir = '/scratch/'
 
-    rouge_fname = 'data.nosync/' + data_split + '/model/'
+    rouge_fname = 'data.nosync/' + data_split + '/small_model/'
 
     if by:
         r = Rouge155(rouge_args='-e /home/kristjan/data1/softwares/rouge/ROUGE/RELEASE-1.5.5/data -c 95 -2 -1 -U -r 1000 -n 4 -w 1.2 -a -m -b 75')
@@ -15,8 +15,8 @@ def run_rouge(data_split='test', by=False, s='system_sent'):
     r.model_dir = rouge_fname
     r.system_dir = 'data.nosync/' + data_split + '/' + s + '/'
 
-    r.system_filename_pattern = 'sum.(\d+).txt'
-    r.model_filename_pattern = 'd_#ID#.txt'
+    r.system_filename_pattern = 's_(\d+).txt'
+    r.model_filename_pattern = 'm_#ID#.txt'
 
     fname = s + '_rouge.out'
     ofp = open(fname, 'w+')
@@ -29,4 +29,4 @@ def run_rouge(data_split='test', by=False, s='system_sent'):
 # output_path_system_segm = 'data.nosync/' + data_split + '/system_segm/'
 
 run_rouge()
-run_rouge(s='system_segm')
+# run_rouge(s='system_segm')
